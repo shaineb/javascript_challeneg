c9baeb744c984d7f118e9a847d2b1375cb654bce
infin <infin@DESKTOP-CGOTLLG>
2021-04-17 01:36:51 -0500

    adding new items

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,35 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+
+
+function createTable(ufoData){ 
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(([key,value]) => {
+     
+        row.append("td").text(value);
+    })
+};
+
+
+data.forEach(createTable);
+
+
+var button = d3.select("#filter-btn")
+button.on("click", function() {
+    d3.event.preventDefault();
+    tbody.html("");
+    var datetimeInput = d3.select("#datetime");
+    var inputvalue = datetimeInput.property("value");
+    var filterdata = data.filter(tbody => tbody.datetime === inputvalue);
+ 
+    if (inputvalue === ""){
+        data.forEach(createTable);  
+    }
+    else { 
+        filterdata.forEach(createTable);
+    }
+  });
+  
